bd4eee21c0e6d32cc482039d3817800e3e30d3b1
Alyona <molokowa.alyona2010@yandex.ru>
2023-02-23 20:58:45 +0300

    add result

---
+++ 2_zadacha/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+
+int[,] result = new int[4, 4];
+FillArrayRandom(result);
+PrintArray(result);
+Console.WriteLine();
+StringMinSum(result);
+
+
+void StringMinSum(int[,] array)
+{
+    int minStr = 0;
+    int minSumStr = 0;
+    int sumstr = 0;
+    for (int i = 0; i < result.GetLength(1); i++)
+    {
+        minStr += result[0, i];
+    }
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++) sumstr += result[i, j];
+        if (sumstr < minStr)
+        {
+            minStr = sumstr;
+            minSumStr = i;
+        }
+        sumstr = 0;
+    }
+    Console.Write($"{minSumStr + 1} строка");
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
---
+++ 4_zadacha/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] arraymassiv = new int[2, 2, 2];
+FillArray(arraymassiv);
+PrintIndex(arraymassiv);
+
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < arraymassiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < arraymassiv.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int h = 0; h < arraymassiv.GetLength(2); h++)
+            {
+                Console.Write($"{arraymassiv[i, j, h]}({i},{j},{h}) ");
+            }
+        }
+    }
+}
+
+void FillArray(int[,,] arr)
+{
+    int count = 10;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int h = 0; h < arr.GetLength(2); h++)
+            {
+                arr[h, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
